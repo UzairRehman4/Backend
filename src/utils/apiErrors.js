2f55867509eea26080d6e1e8dd9dec0ab764329e
Uzair Rehman <rehmanuzair43@gmail.com>
2024-09-05 11:47:43 +0500

    utils api errors code added

--- src/utils/apiErrors.js
+++ src/utils/apiErrors.js
@@ -1,23 +1,43 @@
-class ApiError extends Error {
-  constructor(
-    statusCode,
-    message = "Something went wrong",
-    errors = [],
-    statck = ""
-  ) {
-    super(message);
-    (this.statusCode = statusCode),
-      (this.data = null),
-      (this.message = message),
-      (this.success = false),
-      (this.errors = errors);
+function createApiError(
+  statusCode,
+  message = "Something went wrong",
+  errors = [],
+  stack = ""
+) {
+  const error = new Error(message);
 
-    if (statck) {
-      this.stack = statck;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
-  }
+  return Object.assign(error, {
+    statusCode,
+    data: null,
+    message,
+    success: false,
+    errors,
+    stack: stack || error.stack,
+  });
 }
 
-export { ApiError };
+export { createApiError };
+
+// class ApiError extends Error {
+//   constructor(
+//     statusCode,
+//     message = "Something went wrong",
+//     errors = [],
+//     statck = ""
+//   ) {
+//     super(message);
+//     (this.statusCode = statusCode),
+//       (this.data = null),
+//       (this.message = message),
+//       (this.success = false),
+//       (this.errors = errors);
+
+//     if (statck) {
+//       this.stack = statck;
+//     } else {
+//       Error.captureStackTrace(this, this.constructor);
+//     }
+//   }
+// }
+
+// export { ApiError };
